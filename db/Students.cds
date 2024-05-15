89f6b53a6f00cc55a43186a8dfaa96eba9c95167
Spyridon Pischinas <spischinas@deloitte.gr>
2024-05-15 11:18:43 +0000

    second commit

--- db/Students.cds
+++ db/Students.cds
@@ -1,6 +1,7 @@
 namespace myNamespace;
 
 entity Student{
+   key id: Integer;
    email: String(65);
    first_name: String(20);
    last_name: String(20); 
--- srv/mySimpleSrv.js
+++ srv/mySimpleSrv.js
@@ -7,8 +7,22 @@ const mysrvdemo = (srv) => {
     //srv.on("myfoobar",function(req,res){ return "Hello World!" + req.data.msg ;});  
     
     srv.on("READ", "StudentsSRV", async (req) => {
-      const students = await cds.tx(req).run(SELECT.from(Student));//where({email:"firstmail@gmail.com"}));
-      console.log(students)
+    let students
+      if(req.data.id){students = await cds.tx(req).run(SELECT.from(Student).where({id:req.data.id}));}
+      else{students= await cds.tx(req).run(SELECT.from(Student));}
+    
+        if (req._queryOptions && req._queryOptions.$filter) {
+          const filter = req._queryOptions.$filter;
+          console.log(`$filter: ${filter}`);
+          
+          // Example parsing the $filter query manually (simple case)
+          if (filter.includes('email eq')) {
+            const myemail = filter.match(/email eq '([^']+)'/)[1];  // Extract the email value
+            
+            students = await cds.tx(req).run(SELECT.from(Student).where({email:myemail}))
+          }
+        }
+
       return students;
   });
 
