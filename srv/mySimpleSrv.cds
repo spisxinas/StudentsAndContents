fe955460979c95662d095d8911b463578e930163
Spyridon Pischinas <spischinas@deloitte.gr>
2024-05-15 14:02:26 +0000

    new commit

--- srv/mySimpleSrv.cds
+++ srv/mySimpleSrv.cds
@@ -4,6 +4,7 @@ using myNamespace from '../db/Students';
 service mysrvdemo {
 
     @readonly entity StudentsSRV as projection on myNamespace.Student
+    entity UpdateStudentsSRV as projection on myNamespace.Student
     function myfoobar(msg:String) returns String;
 
 }
--- srv/mySimpleSrv.js
+++ srv/mySimpleSrv.js
@@ -6,27 +6,41 @@ const mysrvdemo = (srv) => {
 
     //srv.on("myfoobar",function(req,res){ return "Hello World!" + req.data.msg ;});  
     
-    srv.on("READ", "StudentsSRV", async (req) => {
-    let students
-      if(req.data.id){students = await cds.tx(req).run(SELECT.from(Student).where({id:req.data.id}));}
-      else{students= await cds.tx(req).run(SELECT.from(Student));}
+  //    srv.on("READ", "StudentsSRV", async (req) => {
+  // //    let students
+  // //   //   if(req.data.id){students = await cds.tx(req).run(SELECT.from(Student).where({id:req.data.id}));}
+  // //     //  students= await cds.tx(req).run(SELECT.from(Student))
     
-        if (req._queryOptions && req._queryOptions.$filter) {
-          const filter = req._queryOptions.$filter;
-          console.log(`$filter: ${filter}`);
+  // //     //   if (req._queryOptions && req._queryOptions.$filter) {
+  // //     //     const filter = req._queryOptions.$filter;
+  // //     //     console.log(`$filter: ${filter}`);
           
-          // Example parsing the $filter query manually (simple case)
-          if (filter.includes('email eq')) {
-            const myemail = filter.match(/email eq '([^']+)'/)[1];  // Extract the email value
+  // //     //     // Example parsing the $filter query manually (simple case)
+  // //     //     if (filter.includes('email eq')) {
+  // //     //       const myemail = filter.match(/email eq '([^']+)'/)[1];  // Extract the email value
             
-            students = await cds.tx(req).run(SELECT.from(Student).where({email:myemail}))
-          }
-        }
+  // //     //       students = await cds.tx(req).run(SELECT.from(Student).where({email:myemail}))
+  // //     //     }
+  // //     //   }
 
-      return students;
-  });
+  // //   return students;
+  // console.log("hello world");
+  //  });
+
+  srv.on("CREATE","UpdateStudentsSRV",async (req,res)=>{
+
+    console.log(req.data.first_name);
+    
+
+    const result = await cds.tx(req).run(UPDATE(Student).set({first_name:req.data.first_name}).where({ id: req.data.id }))
+
+    return req.data;
+
+  })
 
 };
+
+
 
 
  module.exports = mysrvdemo;
