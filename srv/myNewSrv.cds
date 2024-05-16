39f46afdbef08d885b381e098513b7585132d7c8
Spyridon Pischinas <spischinas@deloitte.gr>
2024-05-16 14:17:05 +0000

    students

--- srv/myNewSrv.cds
+++ srv/myNewSrv.cds
@@ -1,10 +1,5 @@
 using myNamespace from '../db/Students';
-using mysrvdemo as mysrvdemo from './mySimpleSrv';
 
 
-extend service mysrvdemo  with @(path: '/hello/myPath'){
-    entity CustomGetStudent as select from myNamespace.Student{
-        *,
-        first_name || ' ' || last_name  as full_name: String
-    }
-    }
+
+   
--- srv/myNewSrv.js
+++ srv/myNewSrv.js
@@ -4,23 +4,6 @@ const { Student } = cds.entities('myNamespace')
 const handlers = (srv) => {
     
 
-    srv.before("CREATE","CustomGetStudent",async (req,res)=>{
-   console.log("hello");
-    //checks email are not personal
-        if(typeof req.data.email === "undefined"){
-            req.error(500,"Email Missing");
-        }
-        if(req.data.email.indexof("gmail") !== -1){
-            req.error(500,"Personal Email Id Not allowed");
-        }
-
-    })
-
-    srv.on("READ","CustomGetStudent",async ()=>{
-        console.log("hello");       
-         })
-
-
 }
 
 module.exports = handlers;
