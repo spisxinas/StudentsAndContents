092c9f1ec1c19143889a5ec71d8167c8d46363e7
Spyridon Pischinas <spischinas@deloitte.gr>
2024-05-16 13:07:22 +0000

    new commit

--- srv/mySimpleSrv.cds
+++ srv/mySimpleSrv.cds
@@ -1,10 +1,22 @@
 using myNamespace from '../db/Students';
 
-@path: '/hello/myPath'
-service mysrvdemo {
+
+service mysrvdemo @(path: '/hello/myPath',impl:'/srv/mySimpleSrv.js') {
 
     @readonly entity StudentsSRV as projection on myNamespace.Student
     entity UpdateStudentsSRV as projection on myNamespace.Student
+    entity InsertStudentSRV as projection on myNamespace.Student
+    entity DeleteStudentSRV as projection on myNamespace.Student
+
     function myfoobar(msg:String) returns String;
 
 }
+
+// extend service mysrvdemo with {
+//     entity CustomGetStudent as select from myNamespace.Student{
+//         *,
+//         first_name || ' ' || last_name  as full_name: String
+//     }excluding{
+//         email
+//     }
+// }
--- srv/mySimpleSrv.js
+++ srv/mySimpleSrv.js
@@ -2,7 +2,7 @@ const cds = require('@sap/cds');
 const { Student } = cds.entities('myNamespace');
 
 
-const mysrvdemo = (srv) => {
+const handlers = (srv) => {
 
     //srv.on("myfoobar",function(req,res){ return "Hello World!" + req.data.msg ;});  
     
@@ -31,17 +31,92 @@ const mysrvdemo = (srv) => {
 
     console.log(req.data.first_name);
     
+    let result = await cds.tx(req)
 
-    const result = await cds.tx(req).run(UPDATE(Student).set({first_name:req.data.first_name}).where({ id: req.data.id }))
+    .run([
+      await UPDATE(Student).set({first_name:req.data.first_name}).where({ id: req.data.id }),
+      await UPDATE(Student).set({last_name:req.data.last_name}).where({id:req.data.id})
+    ])
+    .then((resolve,reject) => {
+      console.log("resolve",resolve);
+      console.log("reject",reject);
+      if(typeof resolve !== "undefined"){
+        return req.data;
+      }else{
+        req.error(409,"Record Not Found");
+      }
+    });
 
     return req.data;
 
   })
 
+
+srv.on("CREATE","InsertStudentSRV",async (req,res)=>{
+
+  console.log(req.data.first_name);
+  
+  let result = await cds.tx(req)
+  .run(
+    INSERT.into(Student).entries({
+     email:req.data.email,
+     first_name:req.data.first_name,
+     last_name:req.data.last_name
+    })
+    )
+  .then((resolve,reject) => {
+    console.log("resolve",resolve);
+    console.log("reject",reject);
+    if(typeof resolve !== "undefined"){
+      return req.data;
+    }else{
+      req.error(409,"Record Not Found");
+    }
+  });
+
+  return req.data;
+
+})
+
+
+srv.on("CREATE","DeleteStudentSRV",async (req,res)=>{
+
+  console.log(req.data.first_name);
+  
+  let result = await cds.tx(req)
+  .run(
+    DELETE.from(Student).where({
+     id: req.data.id
+    })
+    )
+  .then((resolve,reject) => {
+    console.log("resolve",resolve);
+    console.log("reject",reject);
+    if(typeof resolve !== "undefined"){
+      return req.data;
+    }else{
+      req.error(409,"Record Not Found");
+    }
+  });
+
+  return req.data;
+
+})
+
+srv.before("CREATE","InsertStudentSRV",async (req,res)=>{
+  console.log("hello");
+   //checks email are not personal
+       if(typeof req.data.email === "undefined"){
+           req.error(500,"Email Missing");
+       }
+       if(req.data.email.indexOf("gmail") !== -1){
+           req.error(500,"Personal Email Id Not allowed");
+       }
+
+   })
+
 };
 
 
-
-
- module.exports = mysrvdemo;
+ module.exports = handlers;
 
